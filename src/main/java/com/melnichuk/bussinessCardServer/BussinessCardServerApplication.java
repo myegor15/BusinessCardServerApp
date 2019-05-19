26d53155ccb296eb8c00d6edff1aac460d8d5bd0
Yegor Melnichuk <myegor15@gmail.com>
2019-05-19 17:13:37 +0300

    Initial commit

---
+++ src/main/java/com/melnichuk/bussinessCardServer/BussinessCardServerApplication.java
@@ -0,0 +1,13 @@
+package com.melnichuk.bussinessCardServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BussinessCardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BussinessCardServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melnichuk/bussinessCardServer/BussinessCardServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.melnichuk.bussinessCardServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BussinessCardServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
